b98fe5b4364c2e389904aa7ca252d1996aae9f3b
Wreckitshoot <90283922+Wreckitshoot@users.noreply.github.com>
2024-01-23 16:58:30 -0500

    aiming added

---
+++ WB_VerticalShooter/Assets/scripts/pewpew.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pewpew : MonoBehaviour
+{
+    public GameObject PewPewPrefab;
+    public float PewSpeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine( Co_Shootroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //shoot pew
+            shoot();
+        }
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = transform.position.z;
+        transform.up = mouseWorldPos - transform.position;
+    }
+    private void shoot()
+    {
+        //shoot pew
+        GameObject Pew = Instantiate(PewPewPrefab, transform.position, Quaternion.identity);
+        Rigidbody2D PewRB = Pew.GetComponent<Rigidbody2D>();
+        PewRB.velocity = transform.up * PewSpeed;
+        Destroy(PewRB.gameObject, 2.0f);
+    }
+    IEnumerator Co_Shootroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2.0f);
+            shoot();
+        }
+    }
+}
