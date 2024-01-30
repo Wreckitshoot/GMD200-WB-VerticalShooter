ac8628b94d62cc37e027c4de47941fc148b6e3a2
Wreckitshoot <90283922+Wreckitshoot@users.noreply.github.com>
2024-01-30 16:32:49 -0500

    Scoreboard

--- WB_VerticalShooter/Assets/scripts/Enemy_respawn.cs
+++ WB_VerticalShooter/Assets/scripts/Enemy_respawn.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class Enemy_respawn : MonoBehaviour
 {
+    public Score score;
     [SerializeField] int PointValue;
     [SerializeField] GameObject point_prefab;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
-            
-            //GetComponent<Score>().update(PointValue);
+            score.score += PointValue;
             Instantiate(point_prefab,transform.position,transform.rotation);
             other.gameObject.GetComponent<Enemy>().Respawn();
         }
--- WB_VerticalShooter/Assets/scripts/Score.cs
+++ WB_VerticalShooter/Assets/scripts/Score.cs
@@ -7,28 +7,11 @@ using UnityEngine;
 public class Score : MonoBehaviour
 {
     public TextMeshProUGUI scoreboard;
-    public int i = 0;
+    public int score = 0;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
     private void Update()
     {
-        
-        
-    }
-    // Update is called once per frame
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        i++;
-        scoreboard.text = i.ToString();
-    }
-    public void update(int points)
-    {
-        i += points;
-
-        scoreboard.text = "Score: " + i.ToString();
+        scoreboard.text = "Score: " + score.ToString();
     }
 }
