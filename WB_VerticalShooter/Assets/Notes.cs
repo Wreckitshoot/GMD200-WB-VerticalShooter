2a70cfcc500a412bf5a2f1a22213fe26757dadff
Wreckitshoot <90283922+Wreckitshoot@users.noreply.github.com>
2024-01-30 17:03:30 -0500

    Timer

---
+++ WB_VerticalShooter/Assets/Notes.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    /*
+     *change Gravity 
+     * break on amount of hits
+     * duplicate
+     * 
+     * different types
+     * rock:falls fast
+     * eggs: can only be shot 3 times
+     * bombs: no points but minus 5 points if hit grown: when shoot light
+     * Basket ball: bouncy circle
+     * 
+     * 
+     */
+}
---
+++ WB_VerticalShooter/Assets/scripts/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public TextMeshProUGUI timer;
+    [SerializeField] float targetTime = 60.0f;
+
+        void Update()
+        {
+
+            targetTime -= Time.deltaTime;
+            timer.text = targetTime.ToString();
+            if (targetTime <= 0.0f)
+            {
+                timerEnded();
+            }
+
+        }
+
+        void timerEnded()
+        {
+           //new scene
+        }
+
+
+    
+}
